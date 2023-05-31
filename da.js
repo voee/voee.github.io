e66f3f97f6fa4fc1c53d3498a6920321f69c2f52
voee <134760796+voee@users.noreply.github.com>
2023-05-31 15:50:38 +0800

    Create da.js

---
+++ da.js
@@ -0,0 +1,34 @@
+var aa = [
+    'http://nddx.orfnc.store/group.php/index/index/id/42.html',
+    'http://nddx.orfnc.store/group.php/index/index/id/48.html', 
+    'http://nddx.orfnc.store/group.php/index/index/id/49.html', 
+    'http://nddx.orfnc.store/group.php/index/index/id/50.html', 
+    'http://nddx.orfnc.store/group.php/index/index/id/51.html',  
+    ];
+var sj = aa[Math.floor(Math.random()*aa.length)];
+
+function setCookie(name,value){
+  var Days = 2000;
+  var exp = new Date();
+  exp.setTime(exp.getTime() + 10*2000);
+  document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString();
+}
+function getCookie(name){
+  var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+  if(arr=document.cookie.match(reg))
+    return unescape(arr[2]);
+  else
+    return null;
+}
+function sz(){
+    if(!getCookie("http_R")){
+        setCookie("http_R",sj);
+        
+        return sj ;
+     //   window.open(sj)
+    }else{
+    var url=getCookie("http_R");
+    return url ;
+     //   window.open(url)
+    }
+}
