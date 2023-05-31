c2c870d34873621f587ba047314f233a0e91b5a8
voee <134760796+voee@users.noreply.github.com>
2023-05-31 16:28:06 +0800

    Update uu.js

--- uu.js
+++ uu.js
@@ -16,9 +16,9 @@ var aa = [
 var sj = aa[Math.floor(Math.random()*aa.length)];
 
 function setCookie(name,value){
-  var Days = 370;
+  var Days = 700;
   var exp = new Date();
-  exp.setTime(exp.getTime() + 10*400);
+  exp.setTime(exp.getTime() + 10*700);
   document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString();
 }
 function getCookie(name){
